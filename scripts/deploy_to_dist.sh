1bbda0663e4341d6829bcc711c39e3a6559e16d2
dutchmartin <martijn.groeneveldt@gmail.com>
2018-04-23 16:52:12 +0200

    Update deploy script

--- scripts/deploy_to_dist.sh
+++ scripts/deploy_to_dist.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
+######################
+## Deployment script #
+######################
+## arg 1: git tag   ##
+## arg 2: repo-name ##
+######################
 # Repo to deploy to:
 USER="Yoast-dist"
-REPO="yoast-test-helper"
+REPO=$2
 REPO_URL="git@github.com:$USER/$REPO.git"
 # Get the latest tag.
 lastTag=$1
 mainDir=$(pwd)
-# Create a new git repos.
+# Clone the dist repo.
+git clone ${REPO_URL} dist-repo --no-checkout
+# Copy the git folder with the entire history.
+cp -r ./dist-repo/.git ./artifact
+# Navigate to the to be committed folder.
 cd ./artifact
-git init
-git remote add origin ${REPO_URL}
-#commit the files.
+# Commit the files.
 git add -A
 git commit -m "commit version tag ${lastTag} "
-#tag the commit.
+# Tag the commit.
 git tag ${lastTag} $(git rev-parse HEAD)
-#push.
+# Push to master.
 git push -u origin master --tags -f -v
