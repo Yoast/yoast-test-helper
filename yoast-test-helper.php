adf93966a31cf5be4f6bc37c0f7848c5535c7e21
Manuel <manuel@yoast.com>
2019-05-10 14:16:00 +0200

    Update version to 1.2.2

--- yoast-test-helper.php
+++ yoast-test-helper.php
@@ -8,7 +8,7 @@
  *
  * @wordpress-plugin
  * Plugin Name: Yoast Development: Test Helper
- * Version:     1.2.1
+ * Version:     1.2.2
  * Plugin URI:  https://github.com/yoast/yoast-test-helper
  * Description: Utility to provide testing features for Yoast plugins.
  * Author:      Team Yoast
@@ -34,7 +34,7 @@
 define( 'YOAST_TEST_HELPER_FILE', __FILE__ );
 define( 'YOAST_TEST_HELPER_DIR', dirname( YOAST_TEST_HELPER_FILE ) );
 
-define( 'YOAST_TEST_HELPER_VERSION', '1.2.1' );
+define( 'YOAST_TEST_HELPER_VERSION', '1.2.2' );
 
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
