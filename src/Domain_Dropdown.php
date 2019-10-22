78b7e85e9bb3a9ba79236141cd5f9b2e462099d5
Dieter <dieter@yoast.com>
2019-10-22 16:18:48 +0200

    Added functions to replace domain

--- src/Domain_Dropdown.php
+++ src/Domain_Dropdown.php
@@ -35,6 +35,13 @@ class Domain_Dropdown implements Integration {
 	 */
 	public function add_hooks() {
 		add_action( 'admin_post_yoast_seo_domain_dropdown', array( $this, 'handle_submit' ) );
+
+		$domain = $this->option->get('myyoast_test_domain');
+		if ( $domain !== null && $domain !== 'https://my.yoast.com/') {
+			add_action( 'requests-requests.before_request', array ( $this, 'modify_myyoast_request' ), 10, 2 );
+		} else {
+			remove_action( 'requests-requests.before_request', array ( $this, 'modify_myyoast_request' ), 10, 2 );
+		}
 	}
 
 	/**
@@ -44,11 +51,11 @@ class Domain_Dropdown implements Integration {
 	 */
 	public function get_controls() {
 		$select_options = array(
-			'https://my.yoast.com/' => 'live',
-			'https://staging-my.yoast.com' => 'staging',
-			'https://staging-plugins-my.yoast.com/' => 'staging-plugins',
-			'https://staging-platform-my.yoast.com/' => 'staging-platform',
-			'http://my.yoast.test:3000' => 'local',
+			'my.yoast.com/' => 'live',
+			'staging-my.yoast.com' => 'staging',
+			'staging-plugins-my.yoast.com/' => 'staging-plugins',
+			'staging-platform-my.yoast.com/' => 'staging-platform',
+			'my.yoast.test:3000' => 'local',
 		);
 
 		$output = Form_Presenter::create_select(
@@ -67,10 +74,61 @@ class Domain_Dropdown implements Integration {
 	 * @return void
 	 */
 	public function handle_submit() {
-			if ( check_admin_referer( 'yoast_seo_domain_dropdown' ) !== false ) {
+		if ( check_admin_referer( 'yoast_seo_domain_dropdown' ) !== false ) {
 			$this->option->set( 'myyoast_test_domain', $_POST['myyoast_test_domain'] );
 		}
 
 		wp_safe_redirect( self_admin_url( 'tools.php?page=' . apply_filters( 'yoast_version_control_admin_page', '' ) ) );
 	}
+
+	/**
+	 * If a testing domain is set, modify any request to myYoast to go to the testing domain. 
+	 *
+	 * Attached to the `requests-requests.before_request` filter.
+	 * @param string &$url URL of request about to be made
+	 * @param array  &$headers Headers of request about to be made
+	 * @return void
+	 */
+	public function modify_myyoast_request( &$url, &$headers ) {
+		$domain = $this->option->get( 'myyoast_test_domain' );
+		if ( ! $domain || $domain === 'https://my.yoast.com/') {
+			return;
+		}
+
+		$original_url = $url;
+		$request_parameters = $this->replace_domain( $domain, $url, $headers );
+		$url = $request_parameters['url'];
+
+		if ( $request_parameters['host'] ) {
+			$headers['Host'] = $request_parameters['host'];
+			if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
+				error_log( sprintf( "SANDBOXING via '%s': '%s'", $domain, $original_url ) );
+			}
+		}
+	}
+
+	/**
+	 * @param string $domain Testing domain to take place in the request.
+	 * @param string $url URL of request about to be made
+	 * @param array  $headers Headers of request about to be made
+	 * @return array [ 'url' => new URL, 'host' => new Host ]
+	 */
+	function replace_domain( $domain, $url, $headers ) {
+		$host = '';
+		$url_host = wp_parse_url( $url, PHP_URL_HOST );
+
+		switch ( $url_host ) {
+			case 'my.yoast.com' :
+				$host = isset( $headers['Host'] ) ? $headers['Host'] : $url_host;
+				$url = preg_replace(
+					'@^(https?://)' . preg_quote( $url_host, '@' ) . '(?=[/?#].*|$)@',
+					'\\1' . $domain,
+					$url,
+					1
+				);
+		}
+
+		return compact( 'url', 'host' );
+	}
+
 }
