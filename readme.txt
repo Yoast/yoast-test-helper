befb16fcb1885fb17c27b3cfea06f97ea8cc9912
jdevalk <joost@yoast.com>
2020-06-02 15:03:56 +0200

    Updating version to 1.7

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: yoast, joostdevalk, omarreiss, jipmoors, herregroen
 Tags: Yoast, Yoast SEO, development
 Requires at least: 5.4
 Tested up to: 5.4
-Stable tag: 1.6
+Stable tag: 1.7
 Requires PHP: 5.6
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
--- yoast-test-helper.php
+++ yoast-test-helper.php
@@ -8,7 +8,7 @@
  *
  * @wordpress-plugin
  * Plugin Name: Yoast Test Helper
- * Version:     1.6
+ * Version:     1.7
  * Plugin URI:  https://github.com/yoast/yoast-test-helper
  * Description: Utility to provide testing features for Yoast plugins.
  * Author:      Team Yoast
@@ -33,7 +33,7 @@
 
 define( 'YOAST_TEST_HELPER_FILE', __FILE__ );
 define( 'YOAST_TEST_HELPER_DIR', dirname( YOAST_TEST_HELPER_FILE ) );
-define( 'YOAST_TEST_HELPER_VERSION', '1.6' );
+define( 'YOAST_TEST_HELPER_VERSION', '1.7' );
 
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
