61e0c4633460b333501a46db7f3a6215b886038c
Enrico Battocchi <enrico.battocchi@gmail.com>
2024-01-29 09:55:38 +0100

    Fix CS

--- src/indexing-reason-integration.php
+++ src/indexing-reason-integration.php
@@ -28,7 +28,8 @@ class Indexing_Reason_Integration implements Integration {
 		}
 
 		return \sprintf(
-			\esc_html__( 'Because some of your SEO data was reset by the %1$s, your SEO data needs to be reprocessed.', 'yoast-test-helper'  ),
+			/* translators: %1$s: Yoast Test Helper */
+			\esc_html__( 'Because some of your SEO data was reset by the %1$s, your SEO data needs to be reprocessed.', 'yoast-test-helper' ),
 			'Yoast Test Helper'
 		);
 	}
